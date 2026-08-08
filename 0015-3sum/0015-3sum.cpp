0298a7f1000b7d1d41ea7e5a4c2ddc92188bdd4d
Atharva Joshi <155526258+atharvaHJoshi@users.noreply.github.com>
2026-08-09 01:49:09 +0530

    Time: 43 ms (83.69%), Space: 29.1 MB (47.14%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n; i++) {
+            // Optimization: If the lowest number is > 0, three positive numbers can't sum to 0
+            if (nums[i] > 0) break; 
+            
+            // Skip duplicate values for 'i'
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = n - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Move pointers first before checking duplicates
+                    left++;
+                    right--;
+                    
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
