6acaa92469682284ca4f61ccae9d17b8488e246c
Atharva Joshi <155526258+atharvaHJoshi@users.noreply.github.com>
2026-08-11 19:49:42 +0530

    Time: 0 ms (100%), Space: 69.5 MB (66.7%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,36 @@
+// brute force is not working all test cases are satisfying but time limit exceeds 
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        int maxLen= 0 ;
+        int left = 0 , right = 0 ;
+        int zeros  = 0 ;
+        while ( right < n )
+        {
+            if ( nums[right]  == 0 )
+            {
+                zeros++;
+            }
+
+            if ( zeros > k )
+            {
+                if ( nums[left] == 0 )
+                {
+                    zeros--;
+                }
+                left++;
+            }
+
+            if (zeros <= k )
+            {
+                int len = right - left + 1 ;
+                maxLen = max( maxLen , len );
+            }
+
+            right++;
+        }
+        return maxLen;
+    }
+};
