aefd374e38fdd7993a37526d04378f0ce8794414
Atharva Joshi <155526258+atharvaHJoshi@users.noreply.github.com>
2026-08-12 12:53:05 +0530

    Time: 1 ms (75.3%), Space: 10.9 MB (71.38%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,39 @@
+//  better solution with TC of O( N + N)
+
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int left = 0 ;
+        int right = 0 ;
+        int maxLen = 0 ;
+        int maxFreq = 0;
+        vector<int> hashmap(26, 0);
+        while ( right < s.size() )
+        {
+            hashmap[s[right]-'A']++;
+            maxFreq = max(maxFreq, hashmap[s[right] - 'A']);
+
+
+            if ( ((right - left + 1 ) - maxFreq) > k )
+            {
+                hashmap[s[left] - 'A']--;
+                maxFreq = 0;
+                for( int i = 0 ; i < 26 ; i++ )
+                {
+                    maxFreq = max( maxFreq , hashmap[i] );
+                }
+                left = left + 1 ;
+            }
+
+            if ( (right - left + 1 ) - maxFreq <= k )
+            {
+                maxLen = max( maxLen , right - left + 1 );
+            } 
+
+            right++;
+        }
+
+        return maxLen;
+    }
+};
